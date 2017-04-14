aeff802ca7b8d5eb1ed6f164dbb4ebe48f1d3795
Jayanth Chennamangalam <jayanthc@users.noreply.github.com>
2017-04-14 13:12:45 -0700

    Started adding pulsar database support.

--- sdc.py
+++ sdc.py
@@ -1,10 +1,11 @@
 
 import numpy as np
 import scipy as sp
-from flask import Flask
+from flask import Flask, render_template
 from astropy import units as u
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz, get_sun
 from astropy.time import Time
+import csv
 
 app = Flask(__name__)
 
@@ -36,6 +37,25 @@ def obs(ra,de):
     altz = alts.deg - ellim
     riseset = delta_midnight[sp.where(altz[:-1] * altz[1:] < 0)]
     return str(riseset)
+
+@app.route("/pulsars/riseset")
+def showpulsars():
+    pulsar_names = []
+    pulsar_rajd = []
+    pulsar_decjd = []
+    pulsar_risesetlst = []
+    with open("psrcat_small.csv", "r") as csvfile:
+        pulsars = csv.reader(csvfile, delimiter=",")
+        for pulsar in pulsars:
+            pulsar_names.append(pulsar[0])
+            pulsar_rajd.append(pulsar[1])
+            pulsar_decjd.append(pulsar[2])
+            #pulsar_risesetlst.append("test")
+            pulsar_risesetlst.append(obs(pulsar[1], pulsar[2]))
+        num_pulsars = len(pulsar_names)
+        return render_template("pulsars.html", n=num_pulsars, names=pulsar_names,
+                rajd=pulsar_rajd, decjd=pulsar_decjd,
+                risesetlst=pulsar_risesetlst)
 
 if __name__ == "__main__":
     app.run()
